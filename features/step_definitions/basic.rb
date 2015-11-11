bfa9208b0529c1a7a6cf58f4e3d7c59d63de070c
Damien Storm <damien.r.storm@gmail.com>
2015-11-11 18:46:30 +0000

    added placeholder steps

--- features/step_definitions/basic.rb
+++ features/step_definitions/basic.rb
@@ -1,3 +1,15 @@
 Given(/I am on the homepage/) do
   @browser.goto @url
 end
+
+Then(/^I should$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the title should include Stormhold$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the sub\-title should include Welcome to the Storm Family Home$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
