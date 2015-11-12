ac371952e50239f8a78baa44d070ee449706abb5
Damien Storm <damien.r.storm@gmail.com>
2015-11-12 19:25:38 +0000

    refactored to meet rubocop simplicity standards

--- helpers/server_controls.rb
+++ helpers/server_controls.rb
@@ -1,27 +1,20 @@
 require 'colorize'
 
-def start_test_web_server
-	command = ''
-	if !ENV['OS'].nil? && ENV['OS'].include?('Windows')
-		command << 'start '
-	else
-		command << 'screen -d -m -S test_web_server '
-	end
-	command << 'ruby website.rb'
-	command << ' -p $PORT -o $IP' unless ENV['C9_HOSTNAME'].nil?
-
-	Kernel.puts 'Starting web server with command:'.yellow
-	Kernel.puts command.blue
-	result = system(command)
-
-	if result
-	  Kernel.puts 'Server started successfully!'.green
-	  # Kernel.puts 'To access the server screen, use the command:'.yellow
-	  # Kernel.puts 'screen -r test_web_server'.cyan
-	  return true
-	else
-	  Kernel.puts 'There was an issue starting the server!'.red
-	  return false
-	end
+def windows_os?
+  !ENV['OS'].nil? && ENV['OS'].include?('Windows')
 end
 
+def start_test_web_server
+  command = windows_os? ? 'start ' : 'screen -d -m -S test_web_server '
+  command << 'ruby website.rb -p $PORT -o $IP' unless ENV['C9_HOSTNAME'].nil?
+
+  Kernel.puts "Starting web server with command:\n".yellow + command.blue
+
+  result = system(command)
+  if result
+    Kernel.puts 'Server started successfully!'.green
+  else
+    Kernel.puts 'There was an issue starting the server!'.red
+  end
+  result
+end
